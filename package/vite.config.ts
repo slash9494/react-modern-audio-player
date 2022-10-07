671b657305adc43f3412c77a9d6490a28f44d545
slash <slash9494@naver.com>
2022-10-08 07:20:28 +0900

    refactor: remove cjs build file

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "react-modern-audio-player",
-      formats: ["es", "cjs"],
+      formats: ["es"],
       fileName: (format) =>
         format === "es" ? `index.es.js` : `index.${format}`,
     },
